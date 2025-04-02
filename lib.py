87f2ead871b48e8f1c4abcb9b21660d30f7efafb
Saahas Yechuri <saahas.y@gmail.com>
2025-04-01 22:26:40 -0700

    in progress with cleanup

---
+++ lib.py
@@ -0,0 +1,80 @@
+import pyrosim.pyrosim as pyrosim
+from dataclasses import dataclass
+import numpy as np
+
+import pybullet_data
+import pybullet as p
+
+
+class World:
+    def __init__(self, samples=500, urdfs={}, sdfs=[]):
+        physics_client = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+        p.setGravity(0, 0, -50)
+
+        self.samples = samples
+        self.ids = {}
+        self.robots = {}
+        # make urdfs and sdfs
+        for name, urdf in urdfs.items():
+            urdf()
+            id = p.loadURDF(urdf.__name__ + ".urdf")
+            # so that we can have multiple robots with the same form
+            self.ids[name] = id
+            self.robots[name] = {}
+            pyrosim.Prepare_To_Simulate(id)
+
+        for sdf in sdfs:
+            sdf()
+            p.loadSDF(sdf.__name__)
+
+        self.robots["plane"] = p.loadURDF("plane.urdf")
+
+    def __enter__(self):
+        return self
+
+    def step(self):
+        p.stepSimulation()
+
+    def __exit__(self, type, value, traceback):
+        for robotname, senses in self.robots.items():
+            for sense, data in senses.items():
+                np.save(f"data/{robotname}_{sense}.npy", data)
+        p.disconnect()
+
+
+## urdfs
+## some notes:
+# base link and joints all are absolute
+# following links are based on parent joints
+
+# urdf functions will must have the same name as the file they write to
+
+
+def world():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-2, 2, 0.5], size=[1, 1, 1])
+    pyrosim.End()
+
+
+def robot():
+    pyrosim.Start_URDF("robot.urdf")
+    pyrosim.Send_Cube(name="torso", pos=[0, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="rightleg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="leftleg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(
+        name="torso_rightleg",
+        parent="torso",
+        child="rightleg",
+        type="revolute",
+        position=[0.5, 0, 1],
+    )
+    pyrosim.Send_Joint(
+        name="torso_leftleg",
+        parent="torso",
+        child="leftleg",
+        type="revolute",
+        position=[-0.5, 0, 1],
+    )
+    pyrosim.End()
--- sim.py
+++ sim.py
@@ -1,40 +1,33 @@
 #! /Users/saahas/morpho-rl/.venv/bin/python
 import math
 import random
-import pybullet as p
 import numpy as np
-import pyrosim.pyrosim as pyrosim
-import pybullet_data
 import time
 
+import pybullet as p
+import pyrosim.pyrosim as pyrosim
+
+from lib import World, world, robot
+
+
 SAMPLES = 500
-physics_client = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
-p.setGravity(0, 0, -50)
-planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
-pyrosim.Prepare_To_Simulate(robotId)
-p.loadSDF("world.sdf")
-rightleg_data = np.zeros(SAMPLES)
-for i in range(SAMPLES):
-    p.stepSimulation()
-    rightleg_data[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("rightleg")
-    time.sleep(0.01)
-    pyrosim.Set_Motor_For_Joint(
-        bodyIndex=robotId,
-        jointName="torso_rightleg",
-        controlMode=p.POSITION_CONTROL,
-        targetPosition=random.random() * math.pi / 2,
-        maxForce=500,
-    )
-    pyrosim.Set_Motor_For_Joint(
-        bodyIndex=robotId,
-        jointName="torso_leftleg",
-        controlMode=p.POSITION_CONTROL,
-        targetPosition=random.random() * math.pi / 2,
-        maxForce=500,
-    )
-print(rightleg_data)
-np.save("data/rightleg.npy", rightleg_data)
-p.disconnect()
+with World(urdfs={"robot1": robot}, sdfs=[world]) as w:
+    w.robots["rightleg"] = np.zeros(w.samples)
+
+    for i in range(w.samples):
+        w.robots["rightleg"][i] = pyrosim.Get_Touch_Sensor_Value_For_Link("rightleg")
+        time.sleep(0.01)
+        pyrosim.Set_Motor_For_Joint(
+            bodyIndex=robotId,
+            jointName="torso_rightleg",
+            controlMode=p.POSITION_CONTROL,
+            targetPosition=random.random() * math.pi / 2,
+            maxForce=500,
+        )
+        pyrosim.Set_Motor_For_Joint(
+            bodyIndex=robotId,
+            jointName="torso_leftleg",
+            controlMode=p.POSITION_CONTROL,
+            targetPosition=random.random() * math.pi / 2,
+            maxForce=500,
+        )
