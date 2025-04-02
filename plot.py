f76efffd2026ad17658054e0a00caeeca7d1a8b2
Saahas Yechuri <saahas.y@gmail.com>
2025-04-01 18:55:13 -0700

    motors

--- plot.py
+++ plot.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = np.load("data/rightleg.npy")
+
+plt.plot(data)
+plt.show()
--- sim.py
+++ sim.py
@@ -1,4 +1,5 @@
 #! /Users/saahas/morpho-rl/.venv/bin/python
+import math
 import pybullet as p
 import numpy as np
 import pyrosim.pyrosim as pyrosim
@@ -6,7 +7,7 @@ import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
 
-SAMPLES = 100
+SAMPLES = 500
 
 physics_client = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -26,9 +27,22 @@ for i in range(SAMPLES):
     p.stepSimulation()
     rightleg_data[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("rightleg")
     time.sleep(0.01)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = "torso_rightleg",
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = math.pi/4,
+        maxForce = 500)
+    
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = "torso_leftleg",
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = -math.pi/4,
+        maxForce = 500)
 
 print(rightleg_data)
 
-np.save("data/righleg.npy", rightleg_data)
+np.save("data/rightleg.npy", rightleg_data)
 
 p.disconnect()
